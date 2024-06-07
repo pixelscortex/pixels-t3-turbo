54c66ddfa051a00b5ae26102f4d6b17ace73fc7c
justkira <shighgamerhg@gmail.com>
2024-06-07 20:39:37 +0300

    added lint back

--- packages/api/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@repo/eslint-config/base.js";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["dist/**"],
-  },
-  ...baseConfig,
-];
